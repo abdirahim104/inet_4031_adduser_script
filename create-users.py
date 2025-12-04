bb63353d3bf2653e390ffbb2cbd25240fc19c355
abdirahim104 <152966234+abdirahim104@users.noreply.github.com>
2025-12-03 22:31:43 -0600

    Refactor comments and add author details
    Updated comments for clarity and added author information.

--- create-users.py
+++ create-users.py
@@ -1,70 +1,74 @@
 #!/usr/bin/python3
 
 # INET4031
-# Your Name
-# Data Created
-# Date Last Modified
+# Abdirahim Abdullahi
+# 11/28/25
+# 12/3/25
 
-#REPLACE THIS COMMENT - identify what each of these imports is for.
+# os  → used to execute system commands (adduser, passwd, group operations)
+# re  → used for pattern matching to detect commented lines (those beginning with #)
+# sys → used to read input passed via STDIN (the input file redirected into the program)
 import os
 import re
 import sys
 
-#YOUR CODE SHOULD HAVE NONE OF THE INSTRUCTORS COMMENTS REMAINING WHEN YOU ARE FINISHED
-#PLEASE REPLACE INSTRUCTOR "PROMPTS" WITH COMMENTS OF YOUR OWN
-
 def main():
+    # Process each line coming from STDIN (redirected input file)
     for line in sys.stdin:
 
-        #REPLACE THIS COMMENT - this "regular expression" is searching for the presence of a character - what is it and why?
-        #The important part is WHY it is looking for a particular characer - what is that character being used for?
-        match = re.match("^#",line)
+        # Detect whether the line begins with '#' meaning it is a comment and should be skipped
+        match = re.match("^#", line)
 
-        #REPLACE THIS COMMENT - why is the code doing this?
+        # Split the non-comment line into fields separated by colons
         fields = line.strip().split(':')
 
-        #REPLACE THESE COMMENTS with a single comment describing the logic of the IF 
-        #what would an appropriate comment be for describing what this IF statement is checking for?
-        #what happens if the IF statement evaluates to true?
-        #how does this IF statement rely on what happened in the prior two lines of code? The match and fields lines.
-        #the code clearly shows that the variables match and the length of fields is being checked for being != 5  so why is it doing that?
+        # Skip this line if:
+        # 1. It is a comment (match == True), OR
+        # 2. It does not contain exactly 5 fields.
+        #
+        # This ensures only valid user lines are processed. Invalid or incomplete lines
+        # could cause errors if processed, so the script safely ignores them.
         if match or len(fields) != 5:
             continue
 
-        #REPLACE THIS COMMENT - what is the purpose of the next three lines. How does it relate to what is stored in the passwd file?
+        # Extract username, password, and GECOS field components.
+        # The GECOS field stores user information in /etc/passwd:
+        # Format → FirstName LastName,,,
         username = fields[0]
         password = fields[1]
-        gecos = "%s %s,,," % (fields[3],fields[2])
+        gecos = "%s %s,,," % (fields[3], fields[2])
 
-        #REPLACE THIS COMMENT - why is this split being done?
+        # Split the group list into individual group names.
+        # If the field is "-", it means "no groups to add".
         groups = fields[4].split(',')
 
-        #REPLACE THIS COMMENT - what is the point of this print statement?
+        # Inform the user that an account is about to be created
         print("==> Creating account for %s..." % (username))
-        #REPLACE THIS COMMENT - what is this line doing?  What will the variable "cmd" contain.
-        cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos,username)
 
-        #REMOVE THIS COMMENT AFTER YOU UNDERSTAND WHAT TO DO - these statements are currently "commented out" as talked about in class
-        #The first time you run the code...what should you do here?  If uncommented - what will the os.system(cmd) statemetn attempt to do?
-        #print(cmd)
+        # Build the Linux command to create a new user with:
+        # - disabled password (initially)
+        # - GECOS field populated
+        # This prepares the user entry before setting the actual password.
+        cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos, username)
         #os.system(cmd)
 
-        #REPLACE THIS COMMENT - what is the point of this print statement?
+        # Inform the user that a password is being set for the account
         print("==> Setting the password for %s..." % (username))
-        #REPLACE THIS COMMENT - what is this line doing?  What will the variable "cmd" contain. You'll need to lookup what these linux commands do.
-        cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
 
-        #REMOVE THIS COMMENT AFTER YOU UNDERSTAND WHAT TO DO - these statements are currently "commented out" as talked about in class
-        #The first time you run the code...what should you do here?  If uncommented - what will the os.system(cmd) statemetn attempt to do?
-        #print(cmd)
+        # Build the Linux command that:
+        # - Echoes the password twice (as required by passwd)
+        # - Pipes it into the passwd command to set the password
+        cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password, password, username)
         #os.system(cmd)
 
+        # Assign the user to groups listed in the last field
         for group in groups:
-            #REPLACE THIS COMMENT with one that answers "What is this IF statement looking for and why? If group !='-' what happens?"
+
+            # If the group field is "-", it means "no groups to assign"
+            # Otherwise, the user is added to each valid group with adduser <user> <group>
             if group != '-':
-                print("==> Assigning %s to the %s group..." % (username,group))
-                cmd = "/usr/sbin/adduser %s %s" % (username,group)
-                #print(cmd)
+                print("==> Assigning %s to the %s group..." % (username, group))
+                cmd = "/usr/sbin/adduser %s %s" % (username, group)
                 #os.system(cmd)
 
 if __name__ == '__main__':
